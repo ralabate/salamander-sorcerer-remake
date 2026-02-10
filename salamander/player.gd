2e29b06747ff5f16b6f6d208060ee2561acdfdb1
Carlos Quinones <cquinones.private@gmail.com>
2026-02-10 11:26:01 -0500

    quick and dirty sword swipe anim for testing collisions later

--- salamander/player.gd
+++ salamander/player.gd
@@ -4,15 +4,12 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		animation_player.play("swipe")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,7 +19,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0.0
+		velocity.z = 0.0
 
 	move_and_slide()

6bb0aa018048d127727f2377329d6048c76117d2
Carlos Quinones <cquinones.private@gmail.com>
2026-02-11 10:05:24 -0500

    camera tweaks, slowed down the player and rotate to face dir of movement

--- salamander/player.gd
+++ salamander/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 2.5
 
+@onready var visual: MeshInstance3D = %Visual
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 
@@ -11,8 +11,6 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		animation_player.play("swipe")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -21,5 +19,8 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
+
+	if direction:
+		visual.look_at(visual.global_position + direction, Vector3.UP)
 
 	move_and_slide()

180b1b1604fccd8936055b7783be3a7233efb815
Carlos Quinones <cquinones.private@gmail.com>
2026-02-17 11:03:51 -0500

    playing around with pushback

--- salamander/player.gd
+++ salamander/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 3
+@export var speed = 2.5
+@export var push_back = 0.75
 
 @onready var visual: MeshInstance3D = %Visual
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
@@ -9,19 +10,24 @@ const SPEED = 3
 
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		animation_player.play("swipe")
+	if animation_player.current_animation == "swipe":
+		velocity = visual.transform.basis.z * push_back
+		move_and_slide()
+		return
 
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction and animation_player.current_animation != "swipe":
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
 
 	if direction:
 		visual.look_at(visual.global_position + direction, Vector3.UP)
+
+	if Input.is_action_just_pressed("ui_accept"):
+		animation_player.play("swipe")
 
 	move_and_slide()
